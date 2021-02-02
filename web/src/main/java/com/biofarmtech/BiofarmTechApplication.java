601f15e127f6eb87288911d012882f6da93a132d
Chinedu Victor <nwachukwuvictorc@gmil.com>
2021-02-02 12:08:58 +0100

    components added to git

---
+++ web/src/main/java/com/biofarmtech/BiofarmTechApplication.java
@@ -0,0 +1,13 @@
+package com.biofarmtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiofarmTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiofarmTechApplication.class, args);
+    }
+
+}
---
+++ web/src/test/java/com/biofarmtech/BiofarmTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biofarmtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiofarmTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
